ef40105645fb072a366bb57661b74989612fd46f
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-18 20:36:19 +0100

    refactor: Voltando tudo ao modo inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import { MenuLateral } from "./shared/components";
-import { DrawerProvider, AppThemeProvider } from "./shared/contexts";
+
 
 export const App = () => {
   return (
-    <AppThemeProvider>
-      <DrawerProvider>
-        <BrowserRouter>
-          <MenuLateral>
-            <AppRoutes />
-          </MenuLateral>
-        </BrowserRouter>
-      </DrawerProvider>
-    </AppThemeProvider>
+    <div><h1>Olá mundo</h1></div>
   );
 };
--- src/shared/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./menu-lateral/index";
-export * from "./listing-tools/index";
-export * from "./detail-tools/index";
