4b144cdb97754d029a3f53aaf831f322295bd368
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-20 06:18:21 +0100

    refactor: Criando temporizador nas consultas

--- src/pages/peoples/index.tsx
+++ src/pages/peoples/index.tsx
@@ -2,17 +2,21 @@ import { useMemo, useEffect } from "react";
 
 import { useSearchParams } from "react-router-dom";
 import { Toolbar } from "../../shared/components";
+import { useTheBounce } from "../../shared/hooks";
 import { BasePageLayout } from "../../shared/layouts";
 import { PeopleService } from "../../shared/services/api/peoples";
 
 export const ListOfPeople: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { theBounce } = useTheBounce(3000);
+
   const search = useMemo(() => {
     return searchParams.get("search") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PeopleService.getAll(1, search)
+    theBounce(() => {
+      PeopleService.getAll(1, search)
     .then((result) => {
       if(result instanceof Error) {
         alert(result.message)
@@ -20,7 +24,8 @@ export const ListOfPeople: React.FC = () => {
       }
       else console.log(result)
     })
-  }, [search])
+    })
+  }, [theBounce, search])
 
   return (
     <BasePageLayout
