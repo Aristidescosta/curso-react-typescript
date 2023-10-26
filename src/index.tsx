dc406cb594891fee50006d2989017c0582de3b4b
Aristidescosta <aristidiscosta200@gmail.com>
2023-10-26 18:18:14 +0100

    configurando o chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from './shared/chakra-ui-api/theme';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
 );
