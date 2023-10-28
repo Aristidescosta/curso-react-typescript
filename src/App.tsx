5da73cac9f0976d8919a7740b494bc6435659966
Aristidescosta <aristidiscosta200@gmail.com>
2023-10-28 12:16:01 +0100

    refeat: atualizando o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <h1>Olá mundo</h1>
+    <AppRoutes />
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
-export {};
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+export const AppRoutes = () =>{
+    const ROUTER = createBrowserRouter([
+        {
+            path: "/pagina-inicial",
+            element: (
+                <h1>Olá mundo! Elemento principal</h1>
+            ),
+            errorElement: (
+                <h1>Página não encontrada</h1>
+            )
+        }
+    ])
+    return <RouterProvider router={ROUTER} />
+}
